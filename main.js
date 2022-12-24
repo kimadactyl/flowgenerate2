abda699a8a9453e24d211da42e81d97f64f379d5
Dr Kim Foale <kim@gfsc.studio>
2022-12-24 18:53:33 +0000

    Tidy depends

--- main.js
+++ main.js
@@ -2,11 +2,11 @@
 // import javascriptLogo from './javascript.svg'
 // import { setupCounter } from './counter.js'
 
-import * as d3 from 'd3'
+// import * as d3 from 'd3'
 import * as rita from 'rita'
-import { Graphviz } from "@hpcc-js/wasm/graphviz";
+import { Graphviz } from "@hpcc-js/wasm/graphviz"
 
-const graphviz = await Graphviz.load();
+const graphviz = await Graphviz.load()
 
 document.getElementById('app').innerHTML = `
   <h1>Flow Generator 2</h1>
@@ -40,12 +40,10 @@ const dot = `
     g -> h
     h -> a
   }
-`;
+`
 
-const svg = graphviz.circo(dot) // .engine('circo')
+const svg = graphviz.circo(dot)
 
 document.getElementById('graph').innerHTML = svg
 
-
-console.log()
 console.log(graphviz.version());
